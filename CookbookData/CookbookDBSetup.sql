77443bd0fc70dcb7c2671ceeea87865db265c212
thea785 <theawallnc@gmail.com>
2023-01-24 16:36:12 -0500

    Added Ingredients and CreateRecipe stored procedure to database script

--- CookbookData/CookbookDBSetup.sql
+++ CookbookData/CookbookDBSetup.sql
@@ -15,3 +15,28 @@ CREATE TABLE Recipes (
 	[CookTime] int,
 	[Directions] varchar(1000)
 );
+
+CREATE TABLE Ingredients (
+	[IngredientID] int IDENTITY(1,1) PRIMARY KEY,
+	[RecipeID] int FOREIGN KEY REFERENCES Recipes(RecipeID),
+	[Name] varchar(100) NOT NULL,
+	[Amount] int,
+	[Units] varchar(100)
+);
+
+USE Cookbook
+GO
+CREATE PROCEDURE CreateRecipe
+	@Name varchar(100),
+	@Servings int,
+	@PrepTime int,
+	@CookTime int,
+	@Directions varchar(1000),
+	@OutRecipeID int
+AS
+BEGIN
+	INSERT INTO Recipes ([Name], [Servings], [PrepTime], [CookTime], [Directions])
+	VALUES (@Name, @Servings, @PrepTime, @CookTime, @Directions)
+	SELECT @OutRecipeID = SCOPE_IDENTITY();
+END
+GO

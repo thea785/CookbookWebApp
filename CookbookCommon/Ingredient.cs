8fff1f8aa85e92153541e76cba95020f5c0e915a
thea785 <theawallnc@gmail.com>
2023-01-31 16:42:12 -0500

    Added CreateIngredient data method

--- CookbookCommon/Ingredient.cs
+++ CookbookCommon/Ingredient.cs
@@ -2,7 +2,8 @@
 {
     public class Ingredient
     {
-        public int Id { get; set; }
+        public int IngredientID { get; set; }
+        public int RecipeID { get; set; }
         public string? Name { get; set; }
         public int Amount { get; set; }
         public string? Units { get; set; }
---
+++ CookbookData/IngredientsData.cs
@@ -0,0 +1,74 @@
+﻿using CookbookCommon;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CookbookData
+{
+    public static class IngredientsData
+    {
+        const string connString =
+            "Data Source=DESKTOP-GPLJ87I;Initial Catalog=Cookbook;Integrated Security=True";
+
+        public static int CreateIngredient(Ingredient ingredient)
+        {
+            try
+            {
+                int _pk = -1;
+                using (SqlConnection con = new SqlConnection(connString))
+                {
+                    using (SqlCommand _sqlCommand = new SqlCommand("CreateRecipe", con))
+                    {
+                        _sqlCommand.CommandType = CommandType.StoredProcedure;
+                        _sqlCommand.CommandTimeout = 30;
+
+                        SqlParameter _paramRecipeID = _sqlCommand.CreateParameter();
+                        _paramRecipeID.DbType = DbType.String;
+                        _paramRecipeID.ParameterName = "@RecipeID";
+                        _paramRecipeID.Value = ingredient.RecipeID;
+                        _sqlCommand.Parameters.Add(_paramRecipeID);
+
+                        SqlParameter _paramName = _sqlCommand.CreateParameter();
+                        _paramName.DbType = DbType.String;
+                        _paramName.ParameterName = "@Name";
+                        _paramName.Value = ingredient.Name;
+                        _sqlCommand.Parameters.Add(_paramName);
+
+                        SqlParameter _paramAmount = _sqlCommand.CreateParameter();
+                        _paramAmount.DbType = DbType.Int32;
+                        _paramAmount.ParameterName = "@Amount";
+                        _paramAmount.Value = ingredient.Amount;
+                        _sqlCommand.Parameters.Add(_paramAmount);
+
+                        SqlParameter _paramUnits = _sqlCommand.CreateParameter();
+                        _paramUnits.DbType = DbType.Int32;
+                        _paramUnits.ParameterName = "@Units";
+                        _paramUnits.Value = ingredient.Units;
+                        _sqlCommand.Parameters.Add(_paramUnits);
+
+                        SqlParameter _paramIngredientIDReturn = _sqlCommand.CreateParameter();
+                        _paramIngredientIDReturn.DbType = DbType.Int32;
+                        _paramIngredientIDReturn.ParameterName = "@OutIngredientID";
+                        var pk = _sqlCommand.Parameters.Add(_paramIngredientIDReturn);
+                        _paramIngredientIDReturn.Direction = ParameterDirection.Output;
+
+                        con.Open();
+                        _sqlCommand.ExecuteNonQuery(); // calls the sp
+                        _pk = (int)_paramIngredientIDReturn.Value;
+                        con.Close();
+                        return _pk;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                ExceptionLogData.CreateExceptionLog(ex);
+                return -1;
+            }
+        }
+    }
+}

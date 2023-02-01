05a2d62e2ee7b3e35bd6c83032ff693b26030ada
thea785 <theawallnc@gmail.com>
2023-02-01 13:25:40 -0500

    Added GetIngredients data method

--- CookbookData/IngredientsData.cs
+++ CookbookData/IngredientsData.cs
@@ -70,5 +70,47 @@ namespace CookbookData
                 return -1;
             }
         }
+
+        // Returns a list of ingredients for a given recipe
+        public static List<Ingredient> GetIngredients(int recipeID)
+        {
+            List<Ingredient> ingredients = new List<Ingredient>();
+            try
+            {
+                using (SqlConnection dbcon = new SqlConnection(connString))
+                {
+                    using (SqlCommand cmd = new SqlCommand("GetIngredients", dbcon))
+                    {
+                        dbcon.Open(); // Open SqlConnection
+
+                        cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            // Read in Customer records from SqlDataReader
+                            while (reader.Read())
+                            {
+                                Ingredient _ingredient = new Ingredient()
+                                {
+                                    IngredientID = reader["IngredientID"] is DBNull ? 0 : (int)reader["IngredientID"],
+                                    RecipeID = reader["RecipeID"] is DBNull ? 0 : (int)reader["RecipeID"],
+                                    Name = reader["Name"] is DBNull ? "" : (string)reader["Name"],
+                                    Amount = reader["Amount"] is DBNull ? -1 : (int)reader["Amount"],
+                                    Units = reader["Units"] is DBNull ? "" : (string)reader["Units"]
+                                };
+
+                                ingredients.Add(_ingredient);
+                            }
+                        }
+                    }
+                    dbcon.Close();
+                }
+                return ingredients;
+            }
+            catch (Exception ex)
+            {
+                ExceptionLogData.CreateExceptionLog(ex);
+                return ingredients;
+            }
+        }
     }
 }

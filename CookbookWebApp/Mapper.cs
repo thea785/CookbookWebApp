7677f9d09d38b0db68d056995ed8a901ed13cf2f
thea785 <theawallnc@gmail.com>
2023-02-06 15:29:06 -0500

    Started on mapper class to map recipe objects to recipe models

---
+++ CookbookWebApp/Mapper.cs
@@ -0,0 +1,21 @@
+﻿using CookbookCommon;
+using CookbookWebApp.Models;
+
+namespace CookbookWebApp
+{
+    public static class Mapper
+    {
+        public static RecipeModel RecipeToRecipeModel(Recipe r)
+        {
+            return new RecipeModel()
+            {
+                RecipeID= r.RecipeID,
+                Name=r.Name,
+                Servings=r.Servings,
+                PrepTime=r.PrepTime,
+                CookTime=r.CookTime,
+                Directions=r.Directions
+            }
+        }
+    }
+}

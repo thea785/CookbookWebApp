d4d176e2074f2b63f4fe383ed3b328d1cb41bd2f
thea785 <theawallnc@gmail.com>
2023-02-06 12:14:50 -0500

    Created RecipesController outline

---
+++ CookbookWebApp/Controllers/RecipesController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CookbookWebApp.Controllers
+{
+    public class RecipesController : Controller
+    {
+        // Returns index view of recipes
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // Redirect to Create view
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // View details of a given recipe
+        public IActionResult Recipe(int recipeID)
+        {
+            return View();
+        }
+
+        // Delete a given recipe
+        public IActionResult Delete(int recipeID)
+        {
+            return View();
+        }
+    }
+}

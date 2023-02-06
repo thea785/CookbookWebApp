dece80fc658534405eff95d43c1e3cce63630897
thea785 <theawallnc@gmail.com>
2023-02-06 13:09:23 -0500

    Created recipe and ingredient models

---
+++ CookbookWebApp/Models/IngredientModel.cs
@@ -0,0 +1,11 @@
+﻿namespace CookbookWebApp.Models
+{
+    public class IngredientModel
+    {
+        public int IngredientID { get; set; }
+        public int RecipeID { get; set; }
+        public string? Name { get; set; }
+        public int Amount { get; set; }
+        public string? Units { get; set; }
+    }
+}
---
+++ CookbookWebApp/Models/RecipeModel.cs
@@ -0,0 +1,13 @@
+﻿namespace CookbookWebApp.Models
+{
+    public class RecipeModel
+    {
+        public int RecipeID { get; set; }
+        public string? Name { get; set; }
+        public int Servings { get; set; }
+        public int PrepTime { get; set; }
+        public int CookTime { get; set; }
+        public string? Directions { get; set; }
+        public List<IngredientModel>? Ingredients { get; set; }
+    }
+}

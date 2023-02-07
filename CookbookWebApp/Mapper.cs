fd451900f17dfab5bf4d641ba966e0b012e6b599
thea785 <theawallnc@gmail.com>
2023-02-07 12:55:44 -0500

    Implemented recipe to recipemodel mapper function

--- CookbookWebApp/Mapper.cs
+++ CookbookWebApp/Mapper.cs
@@ -7,15 +7,39 @@ namespace CookbookWebApp
     {
         public static RecipeModel RecipeToRecipeModel(Recipe r)
         {
-            return new RecipeModel()
+            // Create a recipe model object to return
+            RecipeModel _rmodel =  new RecipeModel()
             {
-                RecipeID= r.RecipeID,
-                Name=r.Name,
-                Servings=r.Servings,
-                PrepTime=r.PrepTime,
-                CookTime=r.CookTime,
-                Directions=r.Directions
+                RecipeID = r.RecipeID,
+                Name = r.Name,
+                Servings = r.Servings,
+                PrepTime = r.PrepTime,
+                CookTime = r.CookTime,
+                Directions = r.Directions,
+                Ingredients = new List<IngredientModel>()
+            };
+
+            // Check for null ingredients in parameter
+            if (r.Ingredients == null)
+            {
+                return _rmodel;
             }
+
+            // Copy ingredient values into 
+            foreach(Ingredient ingredient in r.Ingredients)
+            {
+                _rmodel.Ingredients.Add(new IngredientModel()
+                {
+                    IngredientID= ingredient.IngredientID,
+                    RecipeID = ingredient.RecipeID,
+                    Name = ingredient.Name,
+                    Amount = ingredient.Amount,
+                    Units= ingredient.Units
+                });
+            }
+
+            // Return the recipe model
+            return _rmodel;
         }
     }
 }

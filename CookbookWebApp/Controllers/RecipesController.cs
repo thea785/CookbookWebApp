6456271d29f88bb8e1b4f6e0961cac60a972ca0e
thea785 <theawallnc@gmail.com>
2023-02-16 12:11:54 -0500

    Implemented Index method in RecipeController

--- CookbookWebApp/Controllers/RecipesController.cs
+++ CookbookWebApp/Controllers/RecipesController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CookbookBLL;
+using CookbookCommon;
+using CookbookWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
 
 namespace CookbookWebApp.Controllers
 {
@@ -7,7 +11,19 @@ namespace CookbookWebApp.Controllers
         // Returns index view of recipes
         public IActionResult Index()
         {
-            return View();
+            // Get Recipes and convert them to RecipeModel objects
+            List<Recipe> recipes = RecipeBLL.GetRecipes();
+            List<RecipeModel> recipeModels = new List<RecipeModel>();
+
+            // Check for null value
+            if (recipes == null) { return View(); }
+
+            foreach(Recipe r in recipes)
+            {
+                recipeModels.Add(Mapper.RecipeToRecipeModel(r));
+            }
+
+            return View(recipeModels);
         }
 
         // Redirect to Create view

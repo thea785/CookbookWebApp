31f2257d0f7e5e8c7d64cee7138b3e6a457643d7
thea785 <theawallnc@gmail.com>
2023-03-06 14:30:34 -0500

    Added required attribute to RecipeModel fields

--- CookbookWebApp/Models/RecipeModel.cs
+++ CookbookWebApp/Models/RecipeModel.cs
@@ -1,12 +1,16 @@
-﻿namespace CookbookWebApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CookbookWebApp.Models
 {
     public class RecipeModel
     {
         public int RecipeID { get; set; }
+        [Required]
         public string? Name { get; set; }
         public int Servings { get; set; }
         public int PrepTime { get; set; }
         public int CookTime { get; set; }
+        [Required]
         public string? Directions { get; set; }
         public List<IngredientModel>? Ingredients { get; set; }
     }

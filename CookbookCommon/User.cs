59804f80113713fd8323e8841c358203d078a269
thea785 <theawallnc@gmail.com>
2023-03-17 09:52:28 -0400

    #3 Added user class in CookbookCommon

---
+++ CookbookCommon/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CookbookCommon
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public int RoleID { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string HashedPassword { get; set; }
+        public string Salt { get; set; }
+    }
+}

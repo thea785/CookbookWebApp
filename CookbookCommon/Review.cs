ca010f750456b12ea77c45874d3e47d2361b3177
thea785 <theawallnc@gmail.com>
2023-04-11 17:42:59 -0400

    #5 Added common class for reviews

---
+++ CookbookCommon/Review.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CookbookCommon
+{
+    public class Review
+    {
+        public int RecipeID { get; set; }
+        public int UserID { get; set; }
+        public string? ReviewText { get; set; }
+    }
+}

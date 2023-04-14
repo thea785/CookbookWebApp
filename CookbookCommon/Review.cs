cdd75cfd932bb59b29e4137f0fad1e6356d972a3
thea785 <theawallnc@gmail.com>
2023-04-14 09:46:23 -0400

    #5 Added review id to review class

--- CookbookCommon/Review.cs
+++ CookbookCommon/Review.cs
@@ -8,6 +8,7 @@ namespace CookbookCommon
 {
     public class Review
     {
+        public int ReviewID { get; set; }
         public int RecipeID { get; set; }
         public int UserID { get; set; }
         public string? ReviewText { get; set; }

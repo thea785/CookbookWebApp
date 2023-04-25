702dfeda585661a4069b701c8aff8e16d1806deb
thea785 <theawallnc@gmail.com>
2023-04-25 10:18:34 -0400

    Closes #9 Implemented review delete button

---
+++ CookbookWebApp/Controllers/ReviewsController.cs
@@ -0,0 +1,14 @@
+﻿using CookbookBLL;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CookbookWebApp.Controllers
+{
+    public class ReviewsController : Controller
+    {
+        public IActionResult DeleteReview(int reviewid, int recipeid)
+        {
+            RecipeBLL.DeleteReview(reviewid);
+            return RedirectToAction("GetRecipe", "Recipes", new { id = recipeid });
+        }
+    }
+}

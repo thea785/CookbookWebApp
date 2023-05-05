9bac5e73317138c76addc877acdec8a8baa725cf
thea785 <theawallnc@gmail.com>
2023-05-05 16:09:58 -0400

    #12 Created FavoritesController

---
+++ CookbookWebApp/Controllers/FavoritesController.cs
@@ -0,0 +1,14 @@
+﻿using CookbookBLL;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CookbookWebApp.Controllers
+{
+    public class FavoritesController : Controller
+    {
+        public IActionResult DeleteFavorite(int recipeID, int userID)
+        {
+            RecipeBLL.DeleteFavorite(recipeID, userID);
+            return RedirectToAction("GetRecipe", "Recipes", new { id = recipeID });
+        }
+    }
+}

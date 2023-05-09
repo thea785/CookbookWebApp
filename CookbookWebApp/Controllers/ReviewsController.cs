8efcf0a772814fd25fa6572b98f08f1c1629f2b9
thea785 <theawallnc@gmail.com>
2023-05-09 14:02:01 -0400

    #11 Implemented CreateReview controller method

--- CookbookWebApp/Controllers/ReviewsController.cs
+++ CookbookWebApp/Controllers/ReviewsController.cs
@@ -1,10 +1,18 @@
 ﻿using CookbookBLL;
+using CookbookCommon;
+using CookbookWebApp.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CookbookWebApp.Controllers
 {
     public class ReviewsController : Controller
     {
+        public IActionResult CreateReview(ReviewModel rm)
+        {
+            Review r = new Review() { RecipeID = rm.RecipeID, ReviewText = rm.ReviewText };
+            RecipeBLL.CreateReview(r);
+            return RedirectToAction("GetRecipe", "Recipes", new { id = rm.RecipeID });
+        }
         public IActionResult DeleteReview(int reviewid, int recipeid)
         {
             RecipeBLL.DeleteReview(reviewid);

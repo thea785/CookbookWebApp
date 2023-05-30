23f1d1c745ff6b2f254db5a13483115d040757f1
thea785 <theawallnc@gmail.com>
2023-05-30 10:15:39 -0400

    #13 Created favorite object for passing data to GetFavorites view

---
+++ CookbookCommon/Favorite.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CookbookCommon
+{
+    public class Favorite
+    {
+        public int RecipeID { get; set; }
+        public string? RecipeName { get; set; }
+    }
+}

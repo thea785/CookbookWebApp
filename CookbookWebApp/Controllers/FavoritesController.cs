cac9806bc86c1c65d894a1303eaf27bb923687aa
thea785 <theawallnc@gmail.com>
2023-06-02 13:51:10 -0400

    #13 Added GetFavorties controller method

--- CookbookWebApp/Controllers/FavoritesController.cs
+++ CookbookWebApp/Controllers/FavoritesController.cs
@@ -1,10 +1,21 @@
 ﻿using CookbookBLL;
 using Microsoft.AspNetCore.Mvc;
+using CookbookWebApp.Models;
+using CookbookCommon;
 
 namespace CookbookWebApp.Controllers
 {
     public class FavoritesController : Controller
     {
+        public IActionResult GetFavorites(int userID)
+        {
+
+            List<Favorite> favorites = new List<Favorite>();
+            RecipeBLL.GetFavorites(userID);
+
+            List<FavoriteModel> favoriteModels = new List<FavoriteModel>();
+            return View(favoriteModels);
+        }
         public IActionResult DeleteFavorite(int recipeID, int userID)
         {
             RecipeBLL.DeleteFavorite(recipeID, userID);

19455c6699e234215dbbc95834c01c073d9e22e6
thea785 <theawallnc@gmail.com>
2023-06-30 17:51:44 -0400

    #18 Created model to be used by Update Password view

---
+++ CookbookWebApp/Models/UpdatePasswordModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CookbookWebApp.Models
+{
+    public class UpdatePasswordModel
+    {
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
